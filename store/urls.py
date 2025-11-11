21715e9ce90e86d23a662f7043a75fbef273aec9
vidalclaravaleri-debug. <vidalclaravaleria@gmail.com>
2025-11-11 12:22:17 -0800

    version mas reciente

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import contact, detail, register
 
-from .views import contact, detail
+from .forms import LoginForm
 
 urlpatterns = [
-    path('contact/',contact, name='contact'),
+    path('contact/', contact, name='contact'),
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html', authentication_form=LoginForm)),
     path('detail/<int:pk>/', detail, name='detail'),
 ]
